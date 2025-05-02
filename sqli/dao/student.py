1685f117989edc688064e0f069713d1eae750c05
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2025-05-02 10:36:59 +0800

    Fix SQL injection vulnerability in student.py

--- sqli/dao/student.py
+++ sqli/dao/student.py
@@ -39,9 +39,9 @@ class Student(NamedTuple):
 
     @staticmethod
     async def create(conn: Connection, name: str):
-        q = ("INSERT INTO students (name) "
-             "VALUES ('%(name)s')" % {'name': name})
+        q = "INSERT INTO students (name) VALUES ($1)"
+
         async with conn.cursor() as cur:
-            await cur.execute(q)
+            await cur.execute(q, name)
 
 
